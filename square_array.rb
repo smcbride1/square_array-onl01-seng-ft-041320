f19c201f37b9e3d79be3f05b2f61f0b492a464b4
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 19:29:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   squared_array = []
   array.each do |item|
-    square_array.push(item ** 2)
+    squared_array.push(item ** 2)
   end
   squared_array
 end
